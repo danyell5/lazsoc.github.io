ea235f20fde62492761f3edb4416533f7e9e869f
karthikbselva <karthikbselva@gmail.com>
2025-11-05 22:36:57 -0500

    Added Sephora

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -13,6 +13,7 @@ const HomePage = () => {
       <HeaderCarousel fetchId={'306Jw6IKAXW2jmyTmiyXlg'} variant="homepage" isTopCarousel={true}/>
       <TextLeftRightImage fetchId={'7b9e9BHrW4lk0EoHL5ZKxU'} variant="homepage"/>
       <ThreeImageLeftTextRight fetchId={'4yUuMfOoMxWiqz95YD6PoE'} variant="homepage"/>
+      <OneTextAboveFourImages fetchId={'79iTGlb6c8VzG3yaRU3M7T'} variant="homepage"/>
       <TwoTextFourImage fetchId={'2igsGQ1nG9AbYozjakL96d'} variant="homepage"/>
       <TwoTextFourImage fetchId={'OlWEYkLjtRxc4ulKoU39H'} variant="carousel"/>
       <FaqBlock fetchId={'3GY0CtaAoFAkRV1j2JaPEL'} variant="homepage"/>
